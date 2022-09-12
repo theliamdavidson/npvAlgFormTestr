831e17cf0ce0b0ec0ced2910f0cdc1bb70cbbb71
theliamdavidson <misterliamd@gmail.com>
2022-09-12 09:55:08 -0700

    minor changes

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
-#include <iostream>
 #include "csv_read.h"
 #include "csv_write.h"
 
 using namespace std;
 
 int main(){
-    
+    char menuOption;
+    cout << "enter menu options: ";
+    cin >> menuOption;
+    switch(menuOption){
+        case('r'):
+            //read
+            cout << 'r';
+            break;
+        case('c'):
+            //CreateAPatient
+            cout << 'c';
+            break;
+        case('w'):
+            // write, will propably block funtionality 
+            // until patient is confirmed existing
+            cout << 'w';
+            break;
+        default:
+            cout << "Option does not exist, please try again";
+            break;
+        
+    }
 }
